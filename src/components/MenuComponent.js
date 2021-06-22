e353d849c923e437bff34c5dc4dcbde7fdc0d432
Jainam Sanghvi <jainam1224@gmail.com>
2021-06-22 15:20:44 +0530

    Functional Components and how to use them

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,70 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
 
-class Menu extends Component {
+function RenderMenuItem ({dish, onClick}) {
+  return (
+      <Card
+          onClick={() => onClick(dish.id)}>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+          </CardImgOverlay>
+      </Card>
+  );
+}
 
-    constructor(props){
-        super(props);
-        // This state helps us to tell about the properties that are used for components
-        this.state = {
-        };
-        console.log('Menu Component constructor is invoked');
-    }
 
-    componentDidMount() {
-      console.log('Menu Component componentDidMount is invoked');
-    }
-    
-    //Media is used for making list of item like menu 
+const Menu = (props) => {
 
-    
-
-  renderDish(dish) {
-      if (dish != null)
-          return(
-              <Card>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-          );
-      else
-          return(
-              <div></div>
-          );
-  }
-  //render method will return the view for the component
-  render() {
-      const menu = this.props.dishes.map((dish) => {
-          return (
-            <div  className="col-12 col-md-5 m-1">
-              <Card key={dish.id}
-                onClick={() => this.props.onClick(dish.id)}>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
-              </Card>
-            </div>
-          );
-      });
-
-      console.log('Menu Component render is invoked');
-
+  const menu = props.dishes.map((dish) => {
       return (
-          <div className="container">
-              <div className="row">
-                  {menu}
-              </div>
-              <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                </div>
-              </div>
+          <div className="col-12 col-md-5 m-1"  key={dish.id}>
+              <RenderMenuItem dish={dish} onClick={props.onClick} />
           </div>
       );
-    }
+  });
+
+  return (
+      <div className="container">
+          <div className="row">
+              {menu}
+          </div>
+      </div>
+  );
 }
+
 
 export default Menu;
