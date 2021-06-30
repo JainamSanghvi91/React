94ab8521bb1ae602b204f8170da1e28bb44bea28
Jainam Sanghvi <jainam1224@gmail.com>
2021-06-30 19:37:10 +0530

    Fetch from server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -6,7 +6,7 @@ import {
 } from "reactstrap";
 import { Loading } from './LoadingComponent';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import { baseUrl } from '../shared/baseUrl.js';
 
 
 
@@ -165,7 +165,7 @@ class CommentForm extends Component {
             return (
                 <div className='col-12 col-md-5 m-1'>
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle> {dish.name}</CardTitle>
                             <CardText> {dish.description} </CardText>
--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,5 +1,6 @@
 import * as ActionTypes from './actionTypes';
 import { DISHES } from '../shared/dishes';
+import { baseUrl } from '../shared/baseUrl.js';
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -11,13 +12,19 @@ export const addComment = (dishId, rating, author, comment) => ({
     }
 });
 
+//DISHES
+
 //This function is a thunk as here inner function is used
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
     //setTimeout is a function which is used to create some delay before our function gets dispached
-    setTimeout(() => {
-        dispatch(addDishes(DISHES));
-    }, 2000);
+    // setTimeout(() => {
+    //     dispatch(addDishes(DISHES));
+    // }, 2000);
+
+    return fetch(baseUrl + 'dishes')
+        .then(response => response.json()) //Here the call back function will convert the data into json format and pass to dishes ni next then below this
+        .then(dishes => dispatch(addDishes(dishes)));    
 }
 
 export const dishesLoading = () => ({
@@ -32,4 +39,46 @@ export const dishesFailed = (errmess) => ({
 export const addDishes = (dishes) => ({
     type: ActionTypes.ADD_DISHES,
     payload: dishes
+});
+
+//COMMENTS
+
+export const fetchComments = () => (dispatch) => {    
+    return fetch(baseUrl + 'comments')
+    .then(response => response.json())
+    .then(comments => dispatch(addComments(comments)));
+};
+
+export const commentsFailed = (errmess) => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errmess
+});
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+//PROMOTIONS
+
+export const fetchPromos = () => (dispatch) => {
+    dispatch(promosLoading());
+
+    return fetch(baseUrl + 'promotions')
+    .then(response => response.json())
+    .then(promos => dispatch(addPromos(promos)));
+}
+
+export const promosLoading = () => ({
+    type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errmess) => ({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errmess
+});
+
+export const addPromos = (promos) => ({
+    type: ActionTypes.ADD_PROMOS,
+    payload: promos
 });
