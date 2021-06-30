6bd67e07d2a909709c4143b7f376511c6d9ca0f0
Jainam Sanghvi <jainam1224@gmail.com>
2021-06-30 22:41:06 +0530

    Animations part - 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -7,6 +7,7 @@ import {
 import { Loading } from './LoadingComponent';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { baseUrl } from '../shared/baseUrl.js';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 
@@ -164,13 +165,19 @@ class CommentForm extends Component {
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
-                    <Card>
-                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle> {dish.name}</CardTitle>
-                            <CardText> {dish.description} </CardText>
-                        </CardBody>
-                    </Card>
+                    <FadeTransform
+                        in
+                        transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                        }}>
+                        <Card>
+                            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </FadeTransform>
                 </div>   
             );
         }
@@ -185,23 +192,18 @@ class CommentForm extends Component {
         if (comments == null) {
             return (<div></div>)
         }
-        const cmnts = comments.map(comment => {
+        const cmnts = <Stagger in>
+        {comments.map((comment) => {
             return (
-                
+                <Fade in>
                 <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author},
-                    &nbsp;
-                    {new Intl.DateTimeFormat('en-US', {
-                        year: 'numeric',
-                        month: 'long',
-                        day: '2-digit'
-                    }).format(new Date(comment.date))}
-                    </p>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                 </li>
-
-            )
-        })
+                </Fade>
+            );
+        })}
+        </Stagger>
         return (
             <div className='col-12 col-md-5 m-1'>
                 <h4> Comments </h4>
